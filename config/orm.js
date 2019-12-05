6fbe9089cda309945986be93c7b0670c305dead8
JC Lenz <jc@miserablepop.com>
2019-12-04 17:51:50 -0800

    Added selectAll(), insertOne(), updateOne() to orm.js

--- config/orm.js
+++ config/orm.js
@@ -2,15 +2,42 @@
 var connection = require("./connection.js");
 
 var orm = {
-    all: function(tableInput, cb) {
-        var queryString = "SELECT * FROM " + tableInput + ";";
-        connection.query(queryString, function(err, result) {
-            if (err) {
-                throw err;
-            }
-            cb(result);
-        });
-    },  
+    
+    //selectAll()
+	selectAll: function(callback) 
+	{
+		//mySQL Query
+		connection.query('SELECT * FROM burgers', function(err, result)
+		{
+			if (err) throw err;
+			callback(result);
+		});
+    },
+    
+    //insertOne()
+	insertOne: function(burger_name, callback)
+	{
+		connection.query('INSERT INTO burgers SET ?', 
+			{	burger_name: burger_name,
+				devoured: false,
+			}, function(err, result)
+			{
+				if (err) throw err;
+				callback(result);
+			});
+				
+	},
+
+	//updateOne()
+	updateOne: function(burgerID, callback)
+	{
+		connection.query('UPDATE burgers SET ? WHERE ?', [{devoured: true}, {id: burgerID}],
+			function(err, result)
+			{
+				if (err) throw err;
+				callback(result);
+			});
+	}
 };
 
 
