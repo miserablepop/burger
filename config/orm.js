7c82ad47515af589349496def286e7ecc5594d8a
JC Lenz <jc@miserablepop.com>
2019-12-04 18:01:48 -0800

    Added functionality to call the ORM functions

--- config/orm.js
+++ config/orm.js
@@ -3,19 +3,19 @@ var connection = require("./connection.js");
 
 var orm = {
     
-    //selectAll()
-	selectAll: function(callback) 
+    // selectAll()
+	selectAll: function(cb) 
 	{
 		//mySQL Query
 		connection.query('SELECT * FROM burgers', function(err, result)
 		{
 			if (err) throw err;
-			callback(result);
+			cb(result);
 		});
     },
-    
-    //insertOne()
-	insertOne: function(burger_name, callback)
+
+    // insertOne()
+	insertOne: function(burger_name, cb)
 	{
 		connection.query('INSERT INTO burgers SET ?', 
 			{	burger_name: burger_name,
@@ -23,23 +23,22 @@ var orm = {
 			}, function(err, result)
 			{
 				if (err) throw err;
-				callback(result);
+				cb(result);
 			});
 				
 	},
 
-	//updateOne()
-	updateOne: function(burgerID, callback)
+	// updateOne()
+	updateOne: function(burgerID, cb)
 	{
 		connection.query('UPDATE burgers SET ? WHERE ?', [{devoured: true}, {id: burgerID}],
 			function(err, result)
 			{
 				if (err) throw err;
-				callback(result);
+				cb(result);
 			});
 	}
 };
-
 
 
 // Export the orm object for the model 
