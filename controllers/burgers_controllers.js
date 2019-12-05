fa803adf3487641a3149fa9ebbe27b15ccbc822c
JC Lenz <jc@miserablepop.com>
2019-12-04 18:05:45 -0800

    Added index redirect and index route to burgers_controllers.js

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -6,6 +6,19 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 
+// Index Redirect
+router.get('/', function (req, res){
+    res.redirect('/index');
+});
+
+// Index Page
+router.get('/index', function(req, res){
+    burger.selectAll(function(data){
+        var hbsObject = {burgers: data};
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
 
 
 
